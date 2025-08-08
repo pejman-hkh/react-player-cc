c6d4a54fdef881cc54f3c6b18dccfa2ff8609935
pejman hkh <pejman.hkh@gmail.com>
2025-08-08 11:26:42 +0330

    Export PlayerInterface

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import ReactPlayerCC from "./ReactPlayerCC";
+import type { PlayerInterface } from "./PlayerInterface";
 
-export { ReactPlayerCC }
+export { ReactPlayerCC };
+export type { PlayerInterface };
 export default ReactPlayerCC
